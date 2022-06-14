1789946c807eac55b5857163183b6746cbae89de
Igor <kudlaev.igor@gmail.com>
2022-06-14 22:20:23 +0300

    feat: added button for refetching data

--- src/features/processing-details/ui/table.tsx
+++ src/features/processing-details/ui/table.tsx
@@ -1,6 +1,8 @@
+import { IconButton } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useStore } from "effector-react";
 import React, { useEffect } from "react";
+import ReplayIcon from "@mui/icons-material/Replay";
 import { $queueInfo, fetchDetailInfoFx } from "../model";
 import styles from "./styles.module.scss";
 
@@ -9,22 +11,54 @@ type PropsType = {
 };
 
 const Table = ({ name }: PropsType) => {
-  // const pending = useStore(fetchDetailInfoFx.pending);
   const queueInfo = useStore($queueInfo);
 
-  useEffect(() => {
+  const fetchData = () => {
     fetchDetailInfoFx({
       name,
     });
+  };
+
+  useEffect(() => {
+    fetchData();
   }, [queueInfo.$page, name]);
 
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "orginalName", headerName: "Name", width: 150 },
-    { field: "status", headerName: "Status" },
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 1,
+      disableColumnMenu: true,
+      sortable: false,
+    },
+    {
+      field: "orginalName",
+      headerName: "Name",
+      flex: 1,
+      disableColumnMenu: true,
+      sortable: false,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      flex: 1,
+      disableColumnMenu: true,
+      sortable: false,
+    },
+    {
+      field: "refetch",
+      filterable: false,
+      disableColumnMenu: true,
+      sortable: false,
+      flex: 1,
+      headerAlign: "right",
+      renderHeader: () => (
+        <IconButton onClick={fetchData}>
+          <ReplayIcon />
+        </IconButton>
+      ),
+    },
   ];
-
-  console.log(queueInfo.$tasks);
 
   return (
     <div className={styles.container}>
